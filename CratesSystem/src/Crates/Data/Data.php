6b86c83aa1d770b2080f0239af33fd5dd3b45827
ImBryMax7u7 <andersonrodriguez.ba@gmail.com>
2020-09-10 13:22:16 -0600

    configuration was updated

---
+++ CratesSystem/src/Crates/Data/Data.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Crates\Data;
+
+use Crates\Loader;
+
+use pocketmine\math\Vector3;
+use pocketmine\level\Level;
+use pocketmine\utils\TextFormat as TE;
+use pocketmine\item\{Item, ItemIds};
+
+class Data {
+	
+}
+
+?>
---
+++ CratesSystem/src/Crates/Loader.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Crates;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat as TE;
+
+use RuntimeException;
+
+class Loader extends PluginBase {
+	
+	/** @var Interface */
+	protected static $instance = null;
+	
+	/**
+	 * @return void
+	 */
+	public function onLoad() : void {
+		self::$instance = $this;
+	}
+	
+	/**
+	 * @return void
+	 */
+	public function onEnable() : void {
+		
+	}
+	
+	/**
+	 * @return void
+	 */
+	public function onDisable() : void {
+		
+	}
+	
+	/**
+	 * @return Config|null
+	 */
+	public static function getDataConfig($configuration) : ?Config {
+		return self::getInstance()->getConfig()->get($configuration);
+	}
+	
+	/**
+	 * @return Config|null
+	 */
+	public static function getConfiguration($configuration) : ?Config {
+		return new Config(self::getInstance()->getDataFolder()."{$configuration}".".yml", Config::YAML);
+	}
+	
+	/**
+	 * Returns values ​​of the main class
+	 * @return Loader[]
+	 */
+	public static function getInstance() : Loader {
+		if(self::$instance === null){
+			throw new RuntimeException("Loader > Could not create instance of variable!");
+		}
+		return self::$instance;
+	}
+}
+
+?>
